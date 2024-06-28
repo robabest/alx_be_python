bad53478fdcadd41ed0631df08cab80988552725
Robel M <robaethioitaly@gmail.com>
2024-06-28 16:03:08 +0200

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,1 +1,11 @@
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
+if weather == 'sunny':
+    print("Wear a t-shirt and sunglasses.")
+elif weather == 'cold':
+    print("Make sure to wear a warm coat and a scarf.")
+elif weather == 'rainy':
+    print("Don’t forget your umbrella and a raincoat.")
+else :
+    print("Sorry, I don't have recommendations for this weather.")
+  
